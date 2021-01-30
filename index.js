c181fb7f6f2c619eaa3974ba37c84e7943df30b0
DefCon-018 <hp761570@gmil.com>
2021-01-30 16:37:38 +0530

    Add template engine

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res){
+    return res.render('home');
+})
 
 app.listen(port, function(err){
     if(err){
