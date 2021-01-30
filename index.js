3ca6f5d4aec600c05f296c559d6425ef20ebbdf2
DefCon-018 <hp761570@gmil.com>
2021-01-30 19:05:11 +0530

    create home page

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const path = require('path');
 const port = 8000;
 const app = express();
 
+app.use(express.urlencoded());
+app.use(express.static('Assets'));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res){
     return res.render('home');
+})
+
+app.post('/create-task', function(req, res){
+    console.log(req.body);
+    return res.redirect('back');
+})
+
+app.post('/test', function(req, res){
+    console.log(req.body);
+    return res.redirect('back');
 })
 
 app.listen(port, function(err){
